92e669c1d04b77d904692e189db3d8d9cd5b5b49
thoma <thoma@Thomas.Home>
2019-12-18 19:47:51 +0000

    Workout project

---
+++ WorkoutProject/src/com/rathinam/Addition.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.rathinam;
+
+/**
+ * @author thoma
+ *
+ */
+public class Addition {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println(Addition.add(10, 10));
+
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+}
